e9d685c02ec25a0228c25f1e9a3a8ea2593fee9e
Gabe Marquez <gabrielmarquez98@gmail.com>
2026-02-13 22:25:42 -0800

    Initial commit

---
+++ 210-lab-6.cpp
@@ -0,0 +1,8 @@
+//*****************************************************************************
+// COMSC-210 | Lab 6 | Gabriel Marquez
+// Description: this program takes a dynamically allocated double array and 
+// outputs and sums it while utilizing pointer notation.
+//*****************************************************************************
+
+#include <iostream>
+using namespace std;
